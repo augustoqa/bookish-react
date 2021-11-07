c00faad9c3712c4700657fe127956bef62922c67
Cesar Acual <augustoacual@gmail.com>
2021-11-07 11:07:14 -0600

    Config tests before execute

--- cypress/integration/ui/bookish.spec.js
+++ cypress/integration/ui/bookish.spec.js
@@ -1,20 +1,51 @@
+import axios from 'axios';
 
 describe('Bookish application', function () {
+  before(() => {
+    return axios
+      .delete('http://localhost:8080/books?_cleanup=true')
+      .catch((err) => err);
+  });
+
+  afterEach(() => {
+    return axios
+      .delete('http://localhost:8080/books?_cleanup=true')
+      .catch((err) => err);
+  });
+
+  beforeEach(() => {
+    const books = [
+      { name: 'Refactoring', id: 1 },
+      { name: 'Domain-driven design', id: 2 },
+      { name: 'Building Microservices', id: 3 },
+    ];
+
+    return books.map((item) =>
+      axios.post('http://localhost:8080/books', item, {
+        headers: { 'Content-Type': 'application/json' },
+      })
+    );
+  });
+
   it('Visits the bookish', function () {
-    cy.visit('http://localhost:3000/')
-    cy.get('h2[data-test="heading"]').contains('Bookish')
-  })
+    cy.visit('http://localhost:3000/');
+    cy.get('h2[data-test="heading"]').contains('Bookish');
+  });
 
   it('Shows a book list', () => {
-    cy.visit('http://localhost:3000/')
-    cy.get('div[data-test="book-list"]').should('exist')
-    cy.get('div.book-item').should('have.length', 2)
+    cy.visit('http://localhost:3000/');
+    cy.get('div[data-test="book-list"]').should('exist');
+    cy.get('div.book-item').should('have.length', 3);
     cy.get('div.book-item').should((books) => {
-      expect(books).to.have.length(2)
+      expect(books).to.have.length(3);
 
-      const titles = [...books].map(x => x.querySelector('h2').innerHTML)
-      console.log(titles)
-      expect(titles).to.deep.equal(['Refactoring', 'Domain-driven design'])
-    })
-  })
-})
+      const titles = [...books].map((x) => x.querySelector('h2').innerHTML);
+      console.log(titles);
+      expect(titles).to.deep.equal([
+        'Refactoring',
+        'Domain-driven design',
+        'Building Microservices',
+      ]);
+    });
+  });
+});
