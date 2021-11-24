5dedbd2fcae0e7e11121359f1a52aff0831b4e9a
Cesar Acual <augustoacual@gmail.com>
2021-11-24 10:34:03 -0600

    Refactoring App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
-import { useState, useEffect } from 'react'
-import Typography from '@material-ui/core/Typography'
-import BookList from './BookList';
-import axios from 'axios'
+import Typography from '@material-ui/core/Typography';
+import BookListContainer from './BookListContainer';
 
 const App = () => {
-  const [books, setBooks] = useState([])
-
-  useEffect(() => {
-    const fetchBooks = async () => {
-      const res = await axios.get('http://localhost:8080/books')
-      setBooks(res.data)
-    }
-    
-    fetchBooks()
-  }, [])
-
   return (
     <div>
       <Typography variant='h2' component='h2' data-test='heading'>
         Bookish
       </Typography>
-      <BookList books={books} />
+      <BookListContainer />
     </div>
   );
-}
+};
 
 export default App;
