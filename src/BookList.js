d0756da62bedf3d3f48c1f0faa3eadd713b71314
Cesar Acual <augustoacual@gmail.com>
2021-11-26 12:04:58 -0600

    Unit testing Loading State, Error State and Expected Data

--- src/BookList.js
+++ src/BookList.js
@@ -1,12 +1,21 @@
-
-const BookList = ({books}) => {
-  return <div data-test='book-list'>
-  {
-    books.map(book => (<div className='book-item'>
-      <h2 className='title'>{book.name}</h2>
-    </div>))
+const BookList = ({ books, loading, error }) => {
+  if (loading) {
+    return <p>Loading...</p>;
   }
-  </div>
-}
 
-export default BookList
+  if (error) {
+    return <p>Error loading books :(</p>;
+  }
+
+  return (
+    <div data-test='book-list'>
+      {books.map((book) => (
+        <div className='book-item' key={book.id}>
+          <h2 className='title'>{book.name}</h2>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default BookList;
