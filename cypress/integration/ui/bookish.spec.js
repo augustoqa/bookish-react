61467f1dfbd9c839130bba03328ffa941fcaa075
Cesar Acual <augustoacual@gmail.com>
2021-12-31 12:52:36 -0600

    Initial data for the App

--- cypress/integration/ui/bookish.spec.js
+++ cypress/integration/ui/bookish.spec.js
@@ -7,26 +7,6 @@ describe('Bookish application', function () {
       .catch((err) => err);
   });
 
-  afterEach(() => {
-    return axios
-      .delete('http://localhost:8080/books?_cleanup=true')
-      .catch((err) => err);
-  });
-
-  beforeEach(() => {
-    const books = [
-      { name: 'Refactoring', id: 1 },
-      { name: 'Domain-driven design', id: 2 },
-      { name: 'Building Microservices', id: 3 },
-    ];
-
-    return books.map((item) =>
-      axios.post('http://localhost:8080/books', item, {
-        headers: { 'Content-Type': 'application/json' },
-      })
-    );
-  });
-
   it('Visits the bookish', function () {
     cy.visit('http://localhost:3000/');
     cy.get('h2[data-test="heading"]').contains('Bookish');
@@ -35,9 +15,9 @@ describe('Bookish application', function () {
   it('Shows a book list', () => {
     cy.visit('http://localhost:3000/');
     cy.get('div[data-test="book-list"]').should('exist');
-    cy.get('div.book-item').should('have.length', 3);
+    cy.get('div.book-item').should('have.length', 4);
     cy.get('div.book-item').should((books) => {
-      expect(books).to.have.length(3);
+      expect(books).to.have.length(4);
 
       const titles = [...books].map((x) => x.querySelector('h2').innerHTML);
       console.log(titles);
@@ -45,6 +25,7 @@ describe('Bookish application', function () {
         'Refactoring',
         'Domain-driven design',
         'Building Microservices',
+        'Acceptance Test Driven Development with React',
       ]);
     });
   });
