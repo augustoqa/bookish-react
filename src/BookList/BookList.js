c9ce26a62d0ee1c5adb49e389f9bd312ffa2eb82
Cesar Acual <augustoacual@gmail.com>
2022-01-11 12:42:06 -0600

    User interface Refinement

--- src/BookList/BookList.js
+++ src/BookList/BookList.js
@@ -1,4 +1,38 @@
+import {
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  Grid,
+  makeStyles,
+  Typography,
+} from '@material-ui/core';
+import { Link } from 'react-router-dom';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+  name: {
+    maxHeight: 30,
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+  },
+  description: {
+    maxHeight: 40,
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+  },
+}));
+
 const BookList = ({ books, loading, error }) => {
+  const classes = useStyles();
   if (loading) {
     return <p>Loading...</p>;
   }
@@ -8,13 +42,40 @@ const BookList = ({ books, loading, error }) => {
   }
 
   return (
-    <div data-test='book-list'>
-      {books.map((book) => (
-        <div className='book-item' key={book.id}>
-          <h2 className='title'>{book.name}</h2>
-          <a href={`/books/${book.id}`}>View Details</a>
-        </div>
-      ))}
+    <div data-test='book-list' className={classes.root}>
+      <Grid container spacing={3}>
+        {books.map((book) => (
+          <Grid item xs={4} sm={4} className='book-item' key={book.id}>
+            <Card>
+              <CardActionArea>
+                <CardContent>
+                  <Typography
+                    gutterBottom
+                    variant='h5'
+                    component='h2'
+                    className={classes.name}
+                  >
+                    {book.name}
+                  </Typography>
+                  <Typography
+                    variant='body2'
+                    color='textSecondary'
+                    component='p'
+                    className={classes.description}
+                  >
+                    {book.description}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button size='small' color='primary'>
+                  <Link to={`/books/${book.id}`}>View Details</Link>
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
     </div>
   );
 };
